f606ab4f6b2acd2da0d77f8987715dc15e5526f3
hzhu <hz@henryzhu.me>
2023-05-18 17:16:50 -0700

    feat: the initial commit
    release-as: 0.0.1

---
+++ src/types.ts
@@ -0,0 +1,69 @@
+export interface ProcessedLog {
+  to: string;
+  from: string;
+  symbol: string;
+  amount: string;
+  address: string;
+  decimals: number;
+}
+
+export interface EnrichedTxReceipt {
+  logs: ProcessedLog[];
+  from: string;
+}
+
+export enum TransactionStatus {
+  REVERTED = 0,
+  SUCCESSFUL = 1,
+}
+
+export interface Log {
+  transactionIndex: number;
+  blockNumber: number;
+  transactionHash: string;
+  address: string;
+  data: string;
+  logIndex?: number;
+  blockHash: string;
+  topics: readonly string[];
+}
+
+export interface EnrichedLogWithoutAmount extends Log {
+  symbol: string;
+  decimals: number;
+  from?: string;
+}
+
+export interface TransactionReceipt {
+  logs: readonly Log[];
+  from: string;
+  status: number | null;
+}
+
+// generic here.
+export interface TxDescription {
+  value: bigint;
+  name: string;
+  args: (string | bigint)[] | (string | bigint)[][];
+  fragment?: {
+    inputs: readonly any[];
+  }
+}
+
+export interface ParseSwapArgs {
+  txReceipt: TransactionReceipt;
+  txDescription: TxDescription;
+  rpcUrl: string;
+}
+
+type ParserFunction = (params: {
+  txDescription: TxDescription;
+  txReceipt: EnrichedTxReceipt;
+}) => {
+  tokenIn: { symbol: string; amount: string };
+  tokenOut: { symbol: string; amount: string };
+} | undefined;
+
+export interface LogParsers {
+  [key: string]: ParserFunction;
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,86 @@
+import type { ProcessedLog } from "../types";
+
+export function convertHexToAddress(hexString: string): string {
+  return `0x${hexString.slice(-40)}`;
+}
+
+export function parseHexDataToString(hexData: string) {
+  const dataLength = parseInt(hexData.slice(66, 130), 16);
+  const data = hexData.slice(130, 130 + dataLength * 2);
+  const bytes: Uint8Array = new Uint8Array(
+    data.match(/.{1,2}/g)?.map((byte: string) => parseInt(byte, 16)) ?? []
+  );
+  const textDecoder = new TextDecoder();
+  const utf8String = textDecoder.decode(bytes);
+
+  return utf8String;
+}
+
+export function formatUnits(data: string, decimals: number) {
+  const bigIntData = BigInt(data);
+  const bigIntDecimals = BigInt(10 ** decimals);
+  const wholePart = bigIntData / bigIntDecimals;
+  const fractionalPart = bigIntData % bigIntDecimals;
+  const paddedFractionalPart = String(fractionalPart).padStart(decimals, "0");
+  const formattedFractionalPart = paddedFractionalPart.replace(/0+$/, "");
+
+  return formattedFractionalPart.length > 0
+    ? `${wholePart}.${formattedFractionalPart}`
+    : wholePart.toString();
+}
+
+async function rpcCall({
+  rpcUrl,
+  method,
+  params = [],
+}: {
+  rpcUrl: string;
+  method: string;
+  params?: unknown[];
+}) {
+  const response = await fetch(rpcUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: 1,
+      method,
+      params,
+    }),
+  });
+
+  return await response.json();
+}
+
+export const erc20Rpc = {
+  getSymbol: async function (contractAddress: string, rpcUrl: string) {
+    const SYMBOL_FUNCTION_HASH = "0x95d89b41";
+    const { result } = await rpcCall({
+      rpcUrl,
+      method: "eth_call",
+      params: [{ to: contractAddress, data: SYMBOL_FUNCTION_HASH }, "latest"],
+    });
+
+    return parseHexDataToString(result);
+  },
+  getDecimals: async function (contractAddress: string, rpcUrl: string) {
+    const DECIMALS_FUNCTION_HASH = "0x313ce567";
+    const { result } = await rpcCall({
+      rpcUrl,
+      method: "eth_call",
+      params: [{ to: contractAddress, data: DECIMALS_FUNCTION_HASH }, "latest"],
+    });
+
+    return Number(BigInt(result));
+  },
+};
+
+export function extractTokenInfo(
+  inputLog: ProcessedLog,
+  outputLog: ProcessedLog
+) {
+  return {
+    tokenIn: { symbol: inputLog.symbol, amount: inputLog.amount },
+    tokenOut: { symbol: outputLog.symbol, amount: outputLog.amount },
+  };
+}

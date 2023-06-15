02eb14497c1437afec2e94a9c52befa451aaa6c6
hzhu <hz@henryzhu.me>
2023-06-15 16:15:44 -0700

    test: add tests for full evm chain coverage

--- src/tests/fillOtcOrder.test.ts
+++ src/tests/fillOtcOrder.test.ts
@@ -1,4 +1,4 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { parseSwap } from "../index";
 import EXCHANGE_PROXY_ABI from "../abi/IZeroEx.json";
 
@@ -10,25 +10,50 @@ if (!ETH_MAINNET_RPC) {
   throw new Error("Missing environment variable `ETH_MAINNET_RPC`");
 }
 
-// https://etherscan.io/tx/0xc9579b9e3cddebd3d48ccb0a719456d7c46869b2c3a536509ea88685c7a5efbb
-it("parses swap from fillOtcOrder", async () => {
-  const data = await parseSwap({
-    transactionHash:
-      "0xc9579b9e3cddebd3d48ccb0a719456d7c46869b2c3a536509ea88685c7a5efbb",
-    exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
-    rpcUrl: ETH_MAINNET_RPC,
+describe("fillOtcOrder", () => {
+  // https://etherscan.io/tx/0xc9579b9e3cddebd3d48ccb0a719456d7c46869b2c3a536509ea88685c7a5efbb
+  it("parse a swap on Ethereum", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0xc9579b9e3cddebd3d48ccb0a719456d7c46869b2c3a536509ea88685c7a5efbb",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: ETH_MAINNET_RPC,
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        symbol: "USDT",
+        amount: "50000",
+        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      },
+      tokenOut: {
+        symbol: "LDO",
+        amount: "21120.508370504671821824",
+        address: "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32",
+      },
+    });
   });
 
-  expect(data).toEqual({
-    tokenIn: {
-      symbol: "USDT",
-      amount: "50000",
-      address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-    },
-    tokenOut: {
-      symbol: "LDO",
-      amount: "21120.508370504671821824",
-      address: "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32",
-    },
+  // https://arbiscan.io/tx/0x3e48c1d1d3596ecfc1f9feb9e9613f5f5fc002b76743251c31eca8bc0aa30e21
+  it("parse a swap on Arbitrum", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0xae78f94319844585bd9e8d4ecfefa2eb70e0cd0d49f76695ee7e4783bad4c1fc",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: "https://1rpc.io/arb",
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        symbol: "ARB",
+        amount: "2050",
+        address: "0x912CE59144191C1204E64559FE8253a0e49E6548",
+      },
+      tokenOut: {
+        symbol: "USDC",
+        amount: "1918.218706",
+        address: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+      },
+    });
   });
 });
--- src/tests/transformERC20.test.ts
+++ src/tests/transformERC20.test.ts
@@ -13,56 +13,6 @@ if (!ETH_MAINNET_RPC) {
   throw new Error("Missing environment variable `ETH_MAINNET_RPC`");
 }
 
-describe("transformERC20 with native input asset", () => {
-  // https://etherscan.io/tx/0x30d015e87dd5481609eec1c54433b8d4679fe641034971baf648d4528a9b0a35
-  it("parses swap from transformERC20", async () => {
-    const data = await parseSwap({
-      transactionHash:
-        "0x30d015e87dd5481609eec1c54433b8d4679fe641034971baf648d4528a9b0a35",
-      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
-      rpcUrl: ETH_MAINNET_RPC,
-    });
-
-    expect(data).toEqual({
-      tokenIn: {
-        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-        symbol: "ETH",
-        amount: "332.666067836453233036",
-      },
-      tokenOut: {
-        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-        symbol: "USDT",
-        amount: "500000.317382",
-      },
-    });
-  });
-});
-
-describe("transformERC20 with erc20 input asset", () => {
-  // https://etherscan.io/tx/0x4db5b7168686cdfb1469b47a67f03fb6199aa81f3d2a26c4a05835b8752d152d
-  it("parses swap from transformERC20", async () => {
-    const data = await parseSwap({
-      transactionHash:
-        "0x4db5b7168686cdfb1469b47a67f03fb6199aa81f3d2a26c4a05835b8752d152d",
-      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
-      rpcUrl: ETH_MAINNET_RPC,
-    });
-
-    expect(data).toEqual({
-      tokenIn: {
-        symbol: "USDT",
-        amount: "275.0",
-        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-      },
-      tokenOut: {
-        symbol: "MUTE",
-        amount: "183.067612917791449241",
-        address: "0xA49d7499271aE71cd8aB9Ac515e6694C755d400c",
-      },
-    });
-  });
-});
-
 describe("transformERC20", () => {
   it("returns undefined when TransformedERC20 topic is not found in logs", async () => {
     const contract = new Contract(
@@ -84,5 +34,168 @@ describe("transformERC20", () => {
     } as any);
 
     expect(result).toBe(undefined);
+  });
+});
+
+describe("transformERC20 on various networks", () => {
+  // https://etherscan.io/tx/0x30d015e87dd5481609eec1c54433b8d4679fe641034971baf648d4528a9b0a35
+  it("Ethereum mainnet: parse a swap with native input asset", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0x30d015e87dd5481609eec1c54433b8d4679fe641034971baf648d4528a9b0a35",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: ETH_MAINNET_RPC,
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        symbol: "ETH",
+        amount: "332.666067836453233036",
+      },
+      tokenOut: {
+        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+        symbol: "USDT",
+        amount: "500000.317382",
+      },
+    });
+  });
+
+  // https://etherscan.io/tx/0x4db5b7168686cdfb1469b47a67f03fb6199aa81f3d2a26c4a05835b8752d152d
+  it("Ethereum mainnet: parse a swap with ERC-20 input asset", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0x4db5b7168686cdfb1469b47a67f03fb6199aa81f3d2a26c4a05835b8752d152d",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: ETH_MAINNET_RPC,
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        symbol: "USDT",
+        amount: "275.0",
+        address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      },
+      tokenOut: {
+        symbol: "MUTE",
+        amount: "183.067612917791449241",
+        address: "0xA49d7499271aE71cd8aB9Ac515e6694C755d400c",
+      },
+    });
+  });
+
+  // https://optimistic.etherscan.io/tx/0x0d8125a0d77af877c5efd475e0b2a8aa7451c2b5b95e2918387f8a038aacd718
+  it("parse a swap on Optimism", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0x0d8125a0d77af877c5efd475e0b2a8aa7451c2b5b95e2918387f8a038aacd718",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: "https://endpoints.omniatech.io/v1/op/mainnet/public",
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        amount: "51.0",
+        symbol: "ETH",
+      },
+      tokenOut: {
+        address: "0x9A601C5bb360811d96A23689066af316a30c3027",
+        amount: "217794.381575767372449449",
+        symbol: "PIKA",
+      },
+    });
+  });
+
+  // https://snowtrace.io/tx/0x989436aff2791d355a08b87c9a97288699ed5a44a75897a963925b1922e12dbb
+  it("parse a swap on Avalanche", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0x989436aff2791d355a08b87c9a97288699ed5a44a75897a963925b1922e12dbb",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: "https://api.avax.network/ext/bc/C/rpc",
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        address: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+        amount: "25650.0",
+        symbol: "USDC",
+      },
+      tokenOut: {
+        address: "0x152b9d0FdC40C096757F570A51E494bd4b943E50",
+        amount: "0.99966847",
+        symbol: "BTC.b",
+      },
+    });
+  });
+
+  // https://ftmscan.com/tx/0xc48de1d0482475d76a13107b4d438605abe0d2223e75167fc46d6d6a54d954c2
+  it("parse a swap on Fantom", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0xc48de1d0482475d76a13107b4d438605abe0d2223e75167fc46d6d6a54d954c2",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: "https://rpc.ftm.tools",
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        amount: "375000.0",
+        symbol: "FTM",
+      },
+      tokenOut: {
+        address: "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75",
+        amount: "126441.837374",
+        symbol: "USDC",
+      },
+    });
+  });
+
+  // https://bscscan.com/tx/0x789d6d51ceb6d32407c97757e4be90c12f789927453413318178bed0ebc53bc0
+  it("parse a swap on BNB Chain", async () => {
+    const data = await parseSwap({
+      transactionHash:
+        "0x789d6d51ceb6d32407c97757e4be90c12f789927453413318178bed0ebc53bc0",
+      exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+      rpcUrl: "https://bscrpc.com",
+    });
+
+    expect(data).toEqual({
+      tokenIn: {
+        address: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+        amount: "400142.7162326532",
+        symbol: "BUSD",
+      },
+      tokenOut: {
+        address: "0x55d398326f99059fF775485246999027B3197955",
+        amount: "399966.973708709289660215",
+        symbol: "USDT",
+      },
+    });
+  });
+});
+
+// https://explorer.celo.org/tx/0x615c5089f772a8f2074750e8c6070013d288af7681435aba1771f6bfc63d1286
+it("parse a swap on Celo", async () => {
+  const data = await parseSwap({
+    transactionHash:
+      "0x615c5089f772a8f2074750e8c6070013d288af7681435aba1771f6bfc63d1286",
+    exchangeProxyAbi: EXCHANGE_PROXY_ABI.compilerOutput.abi,
+    rpcUrl: "https://rpc.ankr.com/celo",
+  });
+
+  expect(data).toEqual({
+    tokenIn: {
+      address: "0x765DE816845861e75A25fCA122bb6898B8B1282a",
+      amount: "500.0",
+      symbol: "cUSD",
+    },
+    tokenOut: {
+      address: "0x74c0C58B99b68cF16A717279AC2d056A34ba2bFe",
+      amount: "23390.112524333224016754",
+      symbol: "SOURCE",
+    },
   });
 });

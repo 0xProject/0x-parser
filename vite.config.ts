5df07a246c85eb89b6df33d8cb362ce33e0d4d9f
hzhu <hz@henryzhu.me>
2024-07-18 12:54:04 -0700

    feat!: migrate project to 0x v2 & settler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, configDefaults } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    testTimeout: 10000,
+    testTimeout: 20000,
     coverage: {
-      exclude: ["examples/**"],
+      include: ["src/index.ts"],
     },
   },
 });

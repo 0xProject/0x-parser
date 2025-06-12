c692dbb9485cc054f4515eaefcae32f05bfba05b
henryzhu.eth <hz@henryzhu.me>
2025-06-11 19:09:32 -0700

    test: update USDC symbol in test for Worldchain

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1309,7 +1309,7 @@ test("parse a swap on Worldchain (USDC.e for WLD) with Settler", async () => {
 
   expect(result).toEqual({
     tokenIn: {
-      symbol: "USDC.e",
+      symbol: "USDC",
       amount: "0.8",
       address: "0x79A02482A880bCE3F13e09Da970dC34db4CD24d1",
     },
@@ -1344,7 +1344,7 @@ test("parse a swap on Worldchain (ETH for USDC.e) with Settler", async () => {
       address: NATIVE_TOKEN_ADDRESS,
     },
     tokenOut: {
-      symbol: "USDC.e",
+      symbol: "USDC",
       amount: "3.332454",
       address: "0x79A02482A880bCE3F13e09Da970dC34db4CD24d1",
     },
